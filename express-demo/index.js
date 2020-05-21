b47137b5b9f01df8fdf4813d924c770da3f20ff8
Joaquin Cid <joaquin@weareadaptive.com>
2020-05-21 18:27:58 +0200

    no message

--- express-demo/index.js
+++ express-demo/index.js
@@ -16,45 +16,72 @@ app.get('/', (req, res)=> {
   res.send('Hello bitch');
 });
 
-
+// GET ALL
 app.get('/api/courses', (req, res)=> {
   res.send(courses);  
 });
 
-
+// GET BY ID
 // /api/courses/1
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
+
+  if (!course) return res.status(404).send(`Course with id ${req.params.id} not found.`);
   
-  if (course) res.send(course);
-  // else res.status(404);
-  else {
-    const verboseMessage = `Course with id ${req.params.id} not found.`;
-    res.status(404).send(verboseMessage);
-  }
-  // res.send(req.params);
+  res.send(course);
 });
 
+// POST
 // Create Course
 app.post('/api/courses', (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-  
-  const result = Joi.validate(req.body, schema);
-
-  if(result.error)  {
-    res.status(400).send(result.error.details[0].message);
+  const { error } = validateCourse(req.body, res); // result.error
+  if (error) {
+    res.status(400).send(error.details[0].message);
     return;
-  } 
+  }
 
   const course = {
     id: courses.length + 1,
     name: req.body.name
   };
+
   courses.push(course);
   res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+  // Validate
+  const { error } = validateCourse(req.body, res); // result.error
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  // Find the course
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send(`Course with id ${req.params.id} not found.`);
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send(`Course with id ${req.params.id} not found.`);
+    
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
+function validateCourse(course){
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+  
+  return Joi.validate(course, schema);
+}
 
 
 // use environment variable called 'port' in case does not exists, 3000
