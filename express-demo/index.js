d22e377bbfeceaa2d1e96379f0eee781b57bd83b
Joaquin Cid <joaquin@weareadaptive.com>
2020-05-21 17:09:45 +0200

    Add validation with npm i joi

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,7 @@
+const Joi = require('joi');
+
 const express = require('express');
 const app = express();
-
 // add json middle ware, allows to read json from body
 app.use(express.json());
 
@@ -36,11 +37,16 @@ app.get('/api/courses/:id', (req, res) => {
 
 // Create Course
 app.post('/api/courses', (req, res) => {
-  if(!req.body.name || req.body.name.length < 3)
-  {
-    res.status(400).send('Name is required and should be minium 3 characters.');
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+  
+  const result = Joi.validate(req.body, schema);
+
+  if(result.error)  {
+    res.status(400).send(result.error.details[0].message);
     return;
-  }
+  } 
 
   const course = {
     id: courses.length + 1,
